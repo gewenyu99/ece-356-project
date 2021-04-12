ff130867e93d48b27950cd87d2645763fb3a177a
Ganashsai Vannithamby <gv.veggie.321@gmail.com>
2021-04-12 15:57:48 -0400

    Added a User Class, couldn't build without it.

---
+++ src/main/java/com/ece356/demographics/model/User.java
@@ -0,0 +1,16 @@
+package com.ece356.demographics.model;
+
+public class User {
+    private String username;
+    private String password;
+    private int enabled;
+
+    public String getUsername(){ return this.username; }
+
+    public void setUsername(String username){ this.username = username; }
+
+    public String getPassword(){ return this.password; }
+    public void setPassword(String password) { this.password = password; }
+
+
+}
